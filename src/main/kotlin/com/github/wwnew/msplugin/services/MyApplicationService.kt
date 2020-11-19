f456d633b2b0c6fcdcac4ca3a10064d4ebb27fdf
GitHub Action <action@github.com>
2020-11-19 02:22:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/wwnew/msplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wwnew/msplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wwnew.msplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wwnew.msplugin.MyBundle
 
 class MyApplicationService {
 
